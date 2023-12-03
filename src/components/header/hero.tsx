08b42bda78e06a4eac28b500734f1d7764ae34c2
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-03 11:35:13 +0600

    Add hero component with logo and video section

---
+++ src/components/header/hero.tsx
@@ -0,0 +1,52 @@
+import { Icons } from "../icons";
+import Link from "next/link";
+import EarthCanvas from "../threed-scenes/Earth";
+
+export default function Hero() {
+  return (
+    <section id="hero" className="max-x-7xl my-5 mx-2 lg:mx-0">
+      <div className="flex flex-col items-center justify-center">
+        <div className="px-3 py-2 border border-primary/30 rounded-full hover:scale-105 transition-transform duration-75 shine shine-hover backdrop-blur-sm">
+          <div className="flex justify-center gap-3 items-center">
+            <Icons.logo width={25} height={25} />
+            <Link href="/#support" className="primary-gradient">
+              Support us build the future
+            </Link>
+          </div>
+        </div>
+      </div>
+      <BigLogo />
+      <h1 className="text-center lg:block font-heading tracking-wider leading-relaxed text-4xl lg:text-5xl">
+        Nurturing Knowledge, Growing Change
+      </h1>
+      <div className="relative">
+        <div className="absolute top-0 left-0 container mx-auto -translate-y-[50%]"></div>
+      </div>
+      <div
+        id="video"
+        className="relative my-6 lg:my-12 flex-col justify-center items-center p-5 h-[300px] lg:h-[700px] translate-y-5 lg:translate-y-20"
+      >
+        <div className="absolute lg:top-[-35%] top-[-25%] inset-0 h-[200px] lg:h-[550px] z-[-5]">
+          <EarthCanvas />
+        </div>
+        <iframe
+          className="w-full h-full"
+          src="https://www.youtube.com/embed/xZwEQJX_JbQ"
+          title="NexusOS - Demo Video from &#39;Cholo Bangladesh&#39; - NASA Space Apps Challenge 2023"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen={true}
+        ></iframe>
+      </div>
+    </section>
+  );
+}
+
+const BigLogo = () => {
+  return (
+    <section id="big-logo" className="flex justify-center items-center">
+      <h1 className="primary-gradient font-heading tracking-wider leading-relaxed text-6xl lg:text-8xl text-justify primary-gradient">
+        GreenLand
+      </h1>
+    </section>
+  );
+};

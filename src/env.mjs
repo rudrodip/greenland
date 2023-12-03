5e7f7512407be1a064d2112e7d6302ab4c61b0d0
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-03 11:32:32 +0600

    Add environment configuration file

---
+++ src/env.mjs
@@ -0,0 +1,27 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+  server: {
+    OPENAI_API_KEY: z.string().min(2),
+    DATABASE_URL: z.string().url().optional(),
+    NEXTAUTH_URL: z.string().url().optional(),
+    NEXTAUTH_SECRET: z.string().min(1),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_ZENODO_API_BASE_URL: z.string().min(1),
+  },
+  runtimeEnv: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_ZENODO_API_BASE_URL: process.env.NEXT_PUBLIC_ZENODO_API_BASE_URL,
+  },
+})

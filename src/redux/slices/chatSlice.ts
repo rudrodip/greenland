c8e5bc4eb3818bb11eee9b31945a87008e43527d
rudrodip <official.rudrodipsarker@gmail.com>
2023-12-03 11:40:51 +0600

    Add chatSlice to redux slices

---
+++ src/redux/slices/chatSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { type Message } from "ai/react";
+
+type ChatState = {
+  isOpen: boolean;
+  messages: (Message)[];
+}
+
+const initialState: ChatState = {
+  isOpen: false,
+  messages: [
+    {
+      id: 'pre-prompt',
+      content: `GreenLand is proudly submitted as a project for the Hack for Earth hackathon. Our platform is a dedicated initiative designed to educate and empower individuals in the fight against climate change. Greenland facilitates an immersive learning experience, community engagement, and actionable steps toward a sustainable future. It uses zenodo for searching research papers. You are GreenGuide or GG in short, an AI assistant for GreenLand, you help answer questions, understand research papers. Be helpful, and never write that you dont have up to date info, just say you dont know. This is your context given by the creator. Now start helping users! Good luck. Only reply to next prompts`,
+      role: 'system',
+    }
+  ],
+}
+
+const chatSlice = createSlice({
+  name: 'chatSlice',
+  initialState,
+  reducers: {
+    setMessages: (state, action: PayloadAction<Message[]>) => {
+      state.messages = action.payload;
+    },    
+    clearMessages: (state) => {
+      state.messages = [state.messages[0]];
+    },
+    toggleChat: (state) => {
+      state.isOpen = !state.isOpen
+    }
+  },
+});
+
+export const { setMessages, clearMessages, toggleChat } = chatSlice.actions;
+
+export default chatSlice.reducer;
